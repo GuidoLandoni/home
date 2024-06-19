01077b044433c748e360b7cee4784c746d47004b
Guido Landoni <Guido_Landoni@itrsa.com.ar>
2024-06-19 10:22:58 -0300

    Agrego view not-found y routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: '**', component: NotFoundComponent}
 ];
